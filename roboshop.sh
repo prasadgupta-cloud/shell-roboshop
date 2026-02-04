e76ac4baf793638122b2e3ba53dfb7f240ed4995
T S M A V P GUPTA <prasad.gupta.tammana@kyndryl.com>
2026-02-04 17:09:24 +0530

    actions

--- roboshop.sh
+++ roboshop.sh
@@ -9,8 +9,8 @@ do
    --image id $AMI_ID \
    --instance-type t3.micro \
    --security-group-ids $SG_ID \
-   --tag-specifications "ResourceType=instance, Tags=[{Key=Name,Value=$instance}]" \
-   --query 'Instances[0],InstanceId' \
+   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+   --query 'Instances[0].InstanceId' \
    --output text )
 
    if [ $instance == "frontend" ]; then
